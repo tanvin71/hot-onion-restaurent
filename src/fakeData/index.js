cfc23784aff9ab9104aee836070b15b7742e6672
tanvin71 <fakrultanvin320@gmail.com>
2020-09-14 14:27:03 +0600

    starting  new project

---
+++ src/fakeData/index.js
@@ -0,0 +1,164 @@
+var foods  = [
+    {
+        "id": 1,
+        "name":"Bread with fruits",
+        "img":"https://i.ibb.co/WkckDNX/breakfast2.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "breakfast"
+
+    },
+    {
+        "id": 2,
+        "name":"kabab",
+        "img":"https://i.ibb.co/x3XYbHZ/breakfast3.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "breakfast"
+
+    },
+    {
+        "id": 3,
+        "name":"Bread and",
+        "img":"https://i.ibb.co/gvxKPJT/breakfast4.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "breakfast"
+
+    },
+    {
+        "id": 4,
+        "name":"",
+        "img":"vhttps://i.ibb.co/9NxBBbX/breakfast5.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "breakfast"
+
+    },
+    {
+        "id": 5,
+        "name":"Bread with fruits",
+        "img":"https://i.ibb.co/WkckDNX/breakfast2.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "breakfast"
+
+    },
+    {
+        "id": 6,
+        "name":"Fried eggs with salad",
+        "img":"https://i.ibb.co/Rgr2LVC/breakfast6.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "breakfast"
+
+    },
+    {
+        "id": 7,
+        "name":"Rice with vegetable",
+        "img":"https://i.ibb.co/x52fmzh/dinner1.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "dinner"
+
+    },
+    {
+        "id": 8,
+        "name":"Fish and salad",
+        "img":"https://i.ibb.co/2q9NdLH/dinner2.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "dinner"
+
+    },
+    {
+        "id": 9,
+        "name":"Fish and vegetable",
+        "img":"https://i.ibb.co/541BTcn/dinner3.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "dinner"
+
+    },
+    {
+        "id": 10,
+        "name":"Maccoroni with yogourt",
+        "img":"https://i.ibb.co/hMc7KZL/dinner4.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "dinner"
+
+    },
+    {
+        "id": 11,
+        "name":"Pure salad",
+        "img":"https://i.ibb.co/6Xzt9pB/dinner5.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "dinner"
+
+    },
+    {
+        "id": 12,
+        "name":"Meat and salad",
+        "img":"https://i.ibb.co/zVCTdqJ/dinner6.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "dinner"
+
+    },
+    {
+        "id": 13,
+        "name":"Salad and meat",
+        "img":"https://i.ibb.co/mqBG6dj/lunch1.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "launch"
+
+    },
+    {
+        "id": 14,
+        "name":"Simple launch",
+        "img":"https://i.ibb.co/B4Wh561/lunch2.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "launch"
+
+    },
+    {
+        "id": 15,
+        "name":"Salad launch",
+        "img":"https://i.ibb.co/PZPxHC9/lunch3.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "launch"
+
+    },
+    {
+        "id": 16,
+        "name":"Rice lauch",
+        "img":"https://i.ibb.co/WDxkXdS/lunch4.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "launch"
+
+    },
+    {
+        "id": 17,
+        "name":"Rice & chicken lauch",
+        "img":"https://i.ibb.co/3mP7GRx/lunch5.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "launch"
+
+    },
+    {
+        "id": 18,
+        "name":"Cheese & salad launch",
+        "img":"https://i.ibb.co/FWjc0Ht/lunch6.png",
+        "price":200,
+        "description":"How we dream about our future",
+        "category": "launch"
+
+    },
+];
